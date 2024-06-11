accc5ed0581fa2d2b33e90ff030e56071907663d
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-06-11 20:00:48 +0300

    fix: change styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,22 +4,14 @@ import React, { forwardRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import DatePicker from "react-datepicker";
-import { FaRegCalendarAlt } from "react-icons/fa";
-import { MdClose } from "react-icons/md";
+import Calendar from "../../public/icons/calendar.svg";
+import Plus from "../../public/icons/plus.svg";
 
 import "react-datepicker/dist/react-datepicker.css";
 
 function Header() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date("2016/12/09"));
-
-  // Функция для форматирования даты
-  const formatDate = (date) => {
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = date.getFullYear();
-    return `${day}-${month}-${year}`;
-  };
 
   const InputWithIcons = forwardRef(
     ({ value, onClick, onChange, clearValue }, ref) => (
@@ -28,18 +20,18 @@ function Header() {
           value={value}
           onClick={onClick}
           onChange={onChange}
-          className="h-7 rounded border text-[13px] leading-[1.2] outline-[#DEDEDE] border-[#DEDEDE] text-[#636264] !pl-2 pr-[29px]" // Добавление padding справа для иконок
-          readOnly // Чтобы предотвратить изменение текста вручную
+          className="h-7 rounded border text-[13px] leading-[1.2] outline-[#DEDEDE] border-[#DEDEDE] text-[#636264] !pl-2 pr-[29px]"
+          readOnly
           placeholder="from"
         />
         <button
           onClick={clearValue}
-          className="absolute right-6 top-0 p-0.5 px-px bg-[#EBEBEB] cursor-pointer border-l border-l-[#DEDEDE] border-r border-r-[#DEDEDE]"
+          className="absolute right-6 top-0 p-0.5 px-px bg-[#EBEBEB] cursor-pointer border-l border-l-[#DEDEDE]"
         >
-          <MdClose className="w-6 h-6 fill-[#5F5F5F]" />
+          <Plus className="w-6 h-6 fill-[#5F5F5F]" />
         </button>
-        <button className="absolute right-0 top-0 p-0.5 px-px bg-[#EBEBEB] cursor-pointer">
-          <FaRegCalendarAlt className=" w-6 h-6 fill-[#5F5F5F]" />
+        <button className="absolute right-0 top-0 p-0.5 px-px bg-[#EBEBEB] cursor-pointer border-l border-l-[#DEDEDE]">
+          <Calendar className=" w-6 h-6 fill-[#5F5F5F]" />
         </button>
       </div>
     )
@@ -55,7 +47,7 @@ function Header() {
           <h1 className="font-montserrat text-black font-medium -tracking-[0.24px] text-[24px] leading-[1.2]">
             monblanproject
           </h1>
-          <p className="text-blue text-[13px] -tracking-[0.39px] leading-[1.2] rounded-sm border border-blue px-3 pt-[6px] pb-[7px]">
+          <p className="text-[#3D8EDA] text-[13px] -tracking-[0.39px] leading-[1.2] rounded-sm border border-[#3D8EDA] px-3 pt-[6px] pb-[7px]">
             Start on 17-02-2016
           </p>
         </div>
@@ -72,27 +64,29 @@ function Header() {
         </ul>
         <div className="flex gap-[11px] items-center">
           <p className="text-base">Date</p>
-          <div className="flex items-center gap-[22px]">
-            <DatePicker
-              selected={startDate}
-              selectsStart
-              startDate={startDate}
-              // selected={selectedDate}
-              onChange={(date) => setStartDate(date)}
-              placeholderText="from"
-              customInput={
-                <InputWithIcons clearValue={() => setStartDate(null)} />
-              }
-            />
-            <DatePicker
-              selected={endDate}
-              selectsEnd
-              startDate={startDate}
-              endDate={endDate}
-              // selected={selectedDate}
-              onChange={(date) => setEndDate(date)}
-              customInput={<InputWithIcons />}
-            />
+          <div className="flex items-center w-[396px] justify-between">
+            <div className="">
+              <DatePicker
+                selected={startDate}
+                selectsStart
+                startDate={startDate}
+                onChange={(date) => setStartDate(date)}
+                placeholderText="from"
+                customInput={
+                  <InputWithIcons clearValue={() => setStartDate(null)} />
+                }
+              />
+            </div>
+            <div className="">
+              <DatePicker
+                selected={endDate}
+                selectsEnd
+                startDate={startDate}
+                endDate={endDate}
+                onChange={(date) => setEndDate(date)}
+                customInput={<InputWithIcons />}
+              />
+            </div>
           </div>
         </div>
       </div>
