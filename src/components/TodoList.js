d35dbe5376d5aaf98341679d5d72f092a5cdc8e7
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-06-11 19:24:21 +0300

    feat: add btn and next data

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -10,10 +10,17 @@ import clsx from "clsx";
 
 function TodoList() {
   const [viewMode, setViewMode] = useState("rows");
+  const [visibleCarts, setVisibleCarts] = useState(8);
+
+  const totalCarts = data.length;
+
+  const loadMore = () => {
+    setVisibleCarts((prevCarts) => Math.min(prevCarts + 8, totalCarts));
+  };
 
   return (
-    <div className="w-[836px] mx-auto pt-[18px]">
-      <div className="flex gap-[21px] justify-end mb-[22px]">
+    <div className="w-[836px] mx-auto pt-[18px] pb-[61px]">
+      <div className="flex gap-[21px] justify-end mb-[23px]">
         <button
           type="button"
           onClick={() => setViewMode("rows")}
@@ -32,13 +39,29 @@ function TodoList() {
       <ul
         className={clsx(
           "grid",
-          viewMode === "rows" ? "gap-2" : "grid-cols-4 gap-y-4 gap-x-2"
+          viewMode === "rows"
+            ? "gap-2 mb-[33px]"
+            : "grid-cols-4 gap-y-4 gap-x-2 mb-6"
         )}
       >
-        {data.map((item, index) => (
+        {data.slice(0, visibleCarts).map((item, index) => (
           <TodoItem viewMode={viewMode} item={item} key={index} />
         ))}
       </ul>
+      <button
+        type="button"
+        onClick={loadMore}
+        disabled={visibleCarts >= totalCarts}
+        className={clsx(
+          "block w-full max-w-[111px] mx-auto uppercase text-[#929292] px-[25px] py-2 rounded-[14px] border font-medium text-[11px] leading-[1.2] -tracking-[0.33px]",
+          {
+            "cursor-not-allowed opacity-50": visibleCarts >= totalCarts,
+            "border-[#929292]": visibleCarts < totalCarts,
+          }
+        )}
+      >
+        {visibleCarts >= totalCarts ? "NO MORE" : "LOAD MORE"}
+      </button>
     </div>
   );
 }
